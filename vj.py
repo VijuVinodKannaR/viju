e46353975418d1076d0dc3fe216c8921dcc1b050
VijuVinodKannaR <167547825+VijuVinodKannaR@users.noreply.github.com>
2024-05-03 15:26:32 +0000

    approved

--- vj.py
+++ vj.py
@@ -21,7 +21,7 @@ LOGGER = get_logger(__name__)
 def run():
     st.set_page_config(
         page_title="Viju",
-        page_icon="👋",
+        page_icon=":flag-no:",
     )
 
 st.image('Viju.png')
